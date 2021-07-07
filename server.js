0bdf42765b3371e910a64a97c30d2ee8e5010e90
David <DRhoton@vinyacastelldefels.com>
2021-07-07 18:04:17 +0200

    App now working locally except for Header Error.  Test now on Heroku.

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var express = require('express');
 var app = express();
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+require('dotenv').config();
+
 // set the local port to 3000
 var port = process.env.PORT || 3000;
 
@@ -15,6 +18,8 @@ var port = process.env.PORT || 3000;
 // so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -55,14 +60,64 @@ app.get("/api/whoami", (req, res) => {
 });
 
 // Project 3: URL Shortener Microservice
-app.get("/api/whoami", (req, res) => {
+
+// create Mongoose schema and model
+const urlSchema = new mongoose.Schema({
+  original_url:  String,
+  short_url:  Number,
+});
+let Url = mongoose.model('Url', urlSchema);
+// create application/json parser
+var jsonParser = bodyParser.json()
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+let shortUrl = 0
+// post the form for the requested url
+app.post("/api/shorturl", urlencodedParser, (req, res) => {
+  console.log("post request called");
+  let originalUrl = req.body.url;
+  shortUrl++;
+  // save requested url into MongoDB
+  let newUrl = new Url ({
+    original_url: originalUrl,
+    short_url: shortUrl
+  });
+  newUrl.save( (err, data) => {
+    if (err) return console.log(err);
+    console.log("newUrl successfully saved");
+    // done(null, data) -> apparently this is not needed
+  });
+  // return JSON object as requested
   res.json({
-    ipaddress : req.ip,
-    language: req.headers["accept-language"],
-    software: req.headers["user-agent"],
+    original_url : originalUrl,
+    short_url : shortUrl
   });
 });
 
+app.get("/api/shorturl/:shortUrl", (req, res) => {
+  console.log("req.params: " + req.params);
+  console.log("req.params.shortUrl: " + req.params.shortUrl);
+  let requestedUrl = Url.findOne({short_url: req.params.shortUrl}, (err, urlFound) => {
+    if (err) return console.log(err);
+    console.log("requestedUrl: " + requestedUrl);
+    console.log("urlFound: " + urlFound);
+    console.log("Original URL: " + urlFound.original_url);
+    res.redirect(urlFound.original_url);
+    //res.redirect(urlFound.original_url);
+    // done(null, urlFound)
+  });
+  /* Url.find({short_url: req.params.shortUrl}).then(foundUrls => {
+    let urlFound = foundUrls[0];
+    console.log("urlFound: " + urlFound);
+    console.log("Original URL: " + urlFound.original_url);
+    //res.redirect(urlFound.original_url);
+  }); */
+
+
+  res.json({
+    smurf: "big smurf"
+  });
+});
 
 // Project 1: create timestamp Microservice
 // Put this one last to avoid the "Invalid Date" message
